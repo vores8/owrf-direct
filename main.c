65c5ddb55055d03e63d353257ba0ce8ce64a6903
vores8 <dbkrasn@gmail.com>
2014-03-14 14:29:49 +0400

    preparing for MP

--- main.c
+++ main.c
@@ -16,150 +16,100 @@
 #include <uart.h>
 
 #include <nrf24l01.h>
-#include "owlib.h"
+#include <debugprint.h>
+#include <owrfreadwrite.h>
+#include <owlib.h>
 
-#include "debugprint.h"
-
-//#define UART_BAUD_SELECT(baudRate, xtalCpu)   ((xtalCpu)/((baudRate)*16l)-1)
-//#define B9600 UART_BAUD_SELECT(9600, F_CPU)
-//
-//void uart_init( void )
-//{
-//  //настройка скорости обмена
-//	  UBRR0H = (unsigned char) (B9600>>8);
-//	  UBRR0L = (unsigned char) B9600;
-//
-//  //8 бит данных, 1 стоп бит, без контроля четности
-//  UCSR0C = ( 1 << USBS0 ) | ( 1 << UCSZ01 ) | ( 1 << UCSZ00 );
-//
-//  //разрешить прием и передачу данных
-//  UCSR0B = ( 1 << TXEN0 ) | ( 1 <<RXEN0 );
-//}
-//
-//unsigned char uart_getc( void )
-//{
-//   //ждем приема байта
-//   while( ( UCSR0A & ( 1 << RXC0 ) ) == 0  );
-//   //считываем принятый байт
-//   return UDR0;
-//}
-//
-//void uart_putc( char c )
-//{
-//  //ждем окончания передачи предыдущего байта
-//  while( ( UCSR0A & ( 1 << UDRE0 ) ) == 0 );
-//  UDR0 = c;
-//}
-
-
-static uint8_t transfer_buf[64];
-
-int doSlave(uint8_t* message) {
-	int i;
+uint8_t doSlave(uint8_t* message) {
+	uint8_t i;
 	uint8_t cmd = message[0];
 	uint8_t datalen = 0;
 	uint8_t retval = 0;
+	debugPrint("command=%x\r\n", cmd);
 	if (cmd == 0x10) {
-		debugPrint("cmd=PING retval=%02X\r\n", retval);
 	} else if (cmd == 0x11) {
-		transfer_buf[1] = OWFirst() ? 1 : 0;
+		message[2] = OWFirst() ? 1 : 0;
 		datalen = 1;
-		debugPrint("cmd=FINDFIRSTDEVICE retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x12) {
-		transfer_buf[1] = OWNext() ? 1 : 0;
+		message[2] = OWNext() ? 1 : 0;
 		datalen = 1;
-		debugPrint("cmd=FINDNEXTDEVICE retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x13) {
 		datalen = 8;
 		for (i = 0; i < 8; i++) {
-			transfer_buf[i + 1] = getROM_NO(i);
+			message[i + 2] = getROM_NO(i);
 		}
-		debugPrint("cmd=GETADDRESS retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x16) {
 		OWTargetSetup(0x00);
-		debugPrint("cmd=SETSEARCHALLDEVICES retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x19) {
-		transfer_buf[1] = OWReadByte();
+		message[2] = OWReadByte();
 		datalen = 1;
-		debugPrint("cmd=READBYTE retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x1B) {
 		uint8_t len;
 		uint8_t* _buf;
 		len = message[1];
 		_buf = message + 2;
 		retval = OWBlock(_buf, len) ? 0x00 : 0xFF;
-		memcpy(transfer_buf + 1, _buf, len);
+		memcpy(message + 2, _buf, len);
 		datalen = len;
-		debugPrint("cmd=DATABLOCK retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x1C) {
-		transfer_buf[1] = OWReset();
+		message[2] = OWReset();
 		datalen = 1;
-		debugPrint("cmd=RESET retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x1D) {
-		OWWriteByte(message[1]);
-		debugPrint("cmd=WRITEBYTEPOWER retval=%02X\r\n",
-				retval);
+//		OWWriteByte(message[1]);
+		uint8_t len;
+		uint8_t* _buf;
+		len = message[1];
+		_buf = message + 2;
+		retval = OWBlock(_buf, len) ? 0x00 : 0xFF;
+		memcpy(message + 2, _buf, len);
+		datalen = len;
 	} else if (cmd == 0x1E) {
-		OWLevel(MODE_NORMAL);
-		debugPrint("cmd=SETPOWERNORMAL retval=%02X\r\n",
-				retval);
+//		OWLevel(MODE_NORMAL);
 	} else if (cmd == 0x1F) {
 		OWTargetSetup(message[1]);
-		debugPrint("cmd=TARGETFAMILYSETUP retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x20) {
 		OWFamilySkipSetup();
-		debugPrint("cmd=TARGETALLFAMILIES retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x21) {
 		uint8_t address[8];
 		memcpy(address, message + 2, 8);
-		transfer_buf[1] = OWSearchROM(address);
+		message[2] = OWSearchROM(address);
 		datalen = 1;
-		debugPrint("cmd=SEARCHROM retval=%02X\r\n",
-				retval);
 	} else if (cmd == 0x22) {
 		uint8_t address[8];
 		memcpy(address, message + 2, 8);
-
-		transfer_buf[1] = OWMatchROM(address);// ? 1 : 0;
+		message[2] = OWMatchROM(address);
 		datalen = 1;
-		debugPrint("cmd=MATCHROM retval=%02X\r\n",
-				retval);
 	} else {
 		retval = 0xFF;
-		debugPrint("default\r\n");
 	}
-	transfer_buf[0] = retval;
+	message[0] = retval;
 	if (retval == 0xFF) {
-		return 1;
+		datalen = 0;
 	}
-//	message[2] = datalen;
-	return datalen + 1;
+	message[1] = datalen;
+	return datalen;
 }
+
+static uint8_t transfer_buf[255];
 
 int bytesRead = 0;
 uint8_t channel = -1;
-uint8_t* msg;
 int phase = 0;
 
 int main(void) {
 
-	msg = transfer_buf;
+	uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(9600, F_CPU));
 
-	uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(57600, F_CPU));
-	setPins(0x10);
+	OWInit(5);
 
-//	nrf24l01_init();
-//	uint8_t addrtx0[NRF24L01_ADDRSIZE] = NRF24L01_ADDRP0;
-//	nrf24l01_settxaddr(addrtx0);
+	nrf24l01_init();
+	nrf24l01_settxaddr((uint8_t*) "OWRF0");
+	nrf24l01_setrxaddr(0, (uint8_t*) "OWRF0");
+	nrf24l01_setrxaddr(1, (uint8_t*) "OWRF1");
+	nrf24l01_setrxaddr(2, (uint8_t*) "OWRF2");
+	nrf24l01_setrxaddr(3, (uint8_t*) "OWRF3");
+	nrf24l01_setrxaddr(4, (uint8_t*) "OWRF4");
+	nrf24l01_setrxaddr(5, (uint8_t*) "OWRF5");
 //	nrf24l01_enabledynamicpayloads();
 
 	sei();
@@ -172,19 +122,18 @@ int main(void) {
 				databyte = uart_getc();
 				if (bytesRead == 0) {
 					if (databyte == 0xDD)
-					transfer_buf[bytesRead++] = databyte;
+						transfer_buf[bytesRead++] = databyte;
 				} else {
 					transfer_buf[bytesRead++] = databyte;
 				}
 			}
 			if (bytesRead == 4) {
 				channel = transfer_buf[1];
-				msg = transfer_buf + 2;
 				debugPrint(
 						"received command channel=%02X cmd=%02X len=%02X\r\n",
-						channel, msg[0], msg[1]);
+						channel, transfer_buf[2], transfer_buf[3]);
 				bytesRead = 0;
-				if (msg[1] == 0) {
+				if (transfer_buf[3] == 0) {
 					phase = 4;
 				} else {
 					phase = 3;
@@ -193,12 +142,12 @@ int main(void) {
 		}
 			break;
 		case 3: {
-			if (bytesRead == msg[1]) {
+			if (bytesRead == transfer_buf[3]) {
 				phase = 4;
 			} else {
 				if (uart_available() > 0) {
 					databyte = uart_getc();
-					*(msg + bytesRead + 2) = databyte;
+					*(transfer_buf + bytesRead + 4) = databyte;
 					bytesRead++;
 				}
 			}
@@ -206,30 +155,75 @@ int main(void) {
 			break;
 		case 4: {
 			if (channel >= 0) {
-				uint8_t tosend[32];
-				memcpy(tosend, msg, msg[1]+2);
-				for (int i = 0; i < msg[1]+2; i++) {
-					debugPrint(">%02X", tosend[i]);
+				uint8_t len = transfer_buf[3] + 3;
+				char* addr = "OWRF1";
+				addr[4] += channel;
+
+				debugPrint("send to %s %d\r\n", addr, len);
+				for (int i = 0; i < len; i++) {
+					debugPrint(">%02X", transfer_buf[i + 1]);
 				}
 				debugPrint("\r\n");
-				int len = doSlave(tosend);
-				uart_putc(channel);
-//				uart_putc(0);
-//				debugPrint("outlen=%d\r\n", len);
-				for (int i = 0; i < len; i++) {
-					uart_putc(transfer_buf[i]);
+
+//				nrf24l01_settxaddr((uint8_t*)"OWRF0");
+//				if (owrf_write(transfer_buf + 1, len ) == 1) {
+					debugPrint("OK\r\n");
+					phase = 5;
+//				} else {
+//					debugPrint("FAIL\r\n");
+//					phase = 6;
+//				}
+			}
+		}
+			break;
+		case 5: {
+//			nrf24l01_setrxaddr(0, (uint8_t*) "OWRF1");
+
+			int length = -1;
+//			length = owrf_read(transfer_buf);
+			unsigned char message[128];
+			memcpy(message, transfer_buf+2, transfer_buf[3] + 2);
+			for (int i = 0; i < transfer_buf[3] + 2; i++) {
+				debugPrint(">%02X", message[i]);
+			}
+			debugPrint("\r\n");
+			length = doSlave(message) + 2;
+			debugPrint("doSlave length=%d\r\n", length);
+//			nrf24l01_setrxaddr(0, (uint8_t*) "OWRF0");
+//			if (length != -1) {
+				transfer_buf[0] = 0;
+				memcpy(transfer_buf+1, message, length);
+				for (int i = 0; i < length; i++) {
 					debugPrint("<%02X", transfer_buf[i]);
 				}
-//				debugPrint("\r\n");
+				debugPrint("\r\n");
+				for (int i = 0; i < length + 1; i++) {
+					uart_putc(transfer_buf[i]);
+				}
 				uart_flush();
-			}
+				phase = 7;
+//			} else {
+//				debugPrint("TIMEOUT\r\n");
+//				phase = 6;
+//			}
+		}
+			break;
+		case 6: {
+			uart_putc(channel);
+			uart_putc(0xFF);
+			uart_putc(0x00);
+//			uart_flush();
 			phase = 0;
 			bytesRead = 0;
-			msg = transfer_buf;
+		}
+			break;
+		case 7: {
+			phase = 0;
+			bytesRead = 0;
+//			uart_flush();
 		}
 			break;
 		}
-
 	}
 }
 
